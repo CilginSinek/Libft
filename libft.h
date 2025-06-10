3550e20351716a25cb11fe02821c3e05ccbdb07b
ismailHanDuman <cilginsinek58@gmail.com>
2025-06-10 16:10:52 +0300

    a

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iduman <iduman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 15:32:25 by iduman            #+#    #+#             */
-/*   Updated: 2025/06/03 18:41:48 by iduman           ###   ########.fr       */
+/*   Updated: 2025/06/10 13:56:18 by iduman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,7 @@ void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
-
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
