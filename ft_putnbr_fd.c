3dfb7bcf381a6ecaaf25fb650913acda5dc74021
ismailHanDuman <cilginsinek58@gmail.com>
2025-06-12 15:49:41 +0300

    update ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: iduman <iduman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 19:45:16 by iduman            #+#    #+#             */
-/*   Updated: 2025/06/02 19:45:16 by iduman           ###   ########.fr       */
+/*   Updated: 2025/06/12 15:47:42 by iduman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,24 @@ Output:
 */
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	long	nbr;
 
 	if (fd < 0)
 		return ;
-	if (n == 0)
-		ft_putchar_fd('0', fd);
+	nbr = n;
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (nbr > 10)
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putchar_fd((nbr % 10) + '0', fd);
+		nbr /= 10;
+	}
 	else
 	{
-		str = ft_itoa(n);
-		if (!str)
-			return ;
-		ft_putstr_fd(str, fd);
-		free(str);
+		ft_putchar_fd(nbr + '0', fd);
 	}
 }
