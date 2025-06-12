0e89a0c6a5763fc9b7d29b26b70ffc6a8c3ad0d8
İsmail Han <cilginsinek58@gmail.com>
2025-06-12 22:59:02 +0300

    update ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -32,7 +32,7 @@ void	ft_putnbr_fd(int n, int fd)
 		nbr *= -1;
 		ft_putchar_fd('-', fd);
 	}
-	if (nbr > 10)
+	if (nbr >= 10)
 	{
 		ft_putnbr_fd(nbr / 10, fd);
 		ft_putchar_fd((nbr % 10) + '0', fd);
