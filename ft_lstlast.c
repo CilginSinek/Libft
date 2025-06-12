2071382698270335161f6e8fd6e0ab73e1482a89
ismailHanDuman <cilginsinek58@gmail.com>
2025-06-12 15:55:59 +0300

    upade bonus

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: iduman <iduman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 17:18:02 by iduman            #+#    #+#             */
-/*   Updated: 2025/06/03 17:21:32 by iduman           ###   ########.fr       */
+/*   Updated: 2025/06/12 15:54:14 by iduman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*new_list;
 
 	new_list = lst;
-	if (new_list->next != NULL)
-		return (ft_lstlast(new_list->next));
+	while (new_list->next != NULL)
+		new_list = new_list->next;
 	return (new_list);
 }
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: iduman <iduman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 17:09:57 by iduman            #+#    #+#             */
-/*   Updated: 2025/06/11 14:23:04 by iduman           ###   ########.fr       */
+/*   Updated: 2025/06/12 15:51:27 by iduman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ int	ft_lstsize(t_list *lst)
 	i = 0;
 	mylst = lst;
 	while (mylst->next != NULL)
+	{
 		i++;
+		mylst = mylst->next;
+	}
 	return (i + 1);
 }
