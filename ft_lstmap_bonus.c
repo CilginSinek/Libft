178fcb161a1d436f190b62571d8ce2f846468067
Ismail <cilginsinek58@gmail.com>
2025-06-15 22:33:32 +0300

    biti sanirim

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -14,27 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*tmp_lst;
-	void	*content;
+	t_list	*new_list;
+	t_list	*new_node;
 
-	if (!lst || !f || !del)
+	 if (!lst || !f)
 		return (NULL);
-	new_lst = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		content = f(lst->content);
-		tmp_lst = ft_lstnew(content);
-		if (!tmp_lst || !content)
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node || !new_node->content)
 		{
-			if (content)
-				del(content);
-			else if (tmp_lst)
-				ft_lstdelone(tmp_lst, del);
-			ft_lstclear(&new_lst, del);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
-	return (new_lst);
+	return (new_list);
 }
